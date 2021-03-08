d6fa8f6ec28c580e42900bfc9e79ee0ff8a857a7
yyzcl <yyzclyang@outlook.com>
2021-03-09 02:08:15 +0800

    chore(eslint): eslint 支持 ts

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,18 @@
 module.exports = {
   extends: ['react-app'],
-  rules: {}
+  rules: {},
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      parserOptions: {
+        project: './tsconfig.json'
+      },
+      extends: ['airbnb-typescript'],
+      rules: {
+        'import/prefer-default-export': 'off',
+        'react/jsx-uses-react': 'off',
+        'react/react-in-jsx-scope': 'off'
+      }
+    }
+  ]
 };
