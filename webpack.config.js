223fc8c9542a8f099cc6cdc9d1922ef7f89c05b4
yyzcl <yyzclyang@outlook.com>
2021-03-09 00:37:38 +0800

    chore(babel): 使用 babel 打包 js

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  entry: './src/index.js'
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
 };
