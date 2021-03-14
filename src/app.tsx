89bfb61def30f5616882c4c9af773d29939b61c0
yyzcl <yyzclyang@outlook.com>
2021-03-14 21:17:25 +0800

    chore(sass): 在 js 中读取 sass 定义的变量

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,11 @@
 import { FC } from 'react';
 import style from './app.module.scss';
+import vars from './vars.module.scss';
 
 const App: FC = () => (
   <>
     <h1 className={style.title}>webpack5 with react</h1>
+    <p style={{ color: vars.danger }}>sass variable example</p>
   </>
 );
 
