2519ec841c073bbf956126851fc694b320edafbc
yyzcl <yyzclyang@outlook.com>
2021-03-16 00:35:02 +0800

    chore(webpack): 将 runtime 抽离出来

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -4,6 +4,9 @@ const baseConfig = require('./webpack.config');
 
 module.exports = merge(baseConfig, {
   mode: 'production',
+  optimization: {
+    runtimeChunk: 'single'
+  },
   plugins: [
     new MiniCssExtractPlugin({
       filename: 'static/css/index.[contenthash].css'
