2807e58b0c42a4f7f47d650d0eb439a59d3b799e
yyzcl <yyzclyang@outlook.com>
2021-03-17 00:03:38 +0800

    chore(webpack): 抽离 node_modules 下的依赖

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -5,7 +5,20 @@ const baseConfig = require('./webpack.config');
 module.exports = merge(baseConfig, {
   mode: 'production',
   optimization: {
-    runtimeChunk: 'single'
+    runtimeChunk: 'single',
+    moduleIds: 'deterministic', // 将模块 id 固定
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          minSize: 0, // 只要是 node_modules 里面的就抽出来
+          maxSize: 2000000, // 最大 200kb，再大就分包
+          priority: 10,
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all' // all 表示同步加载和异步加载，async 表示异步加载，initial 表示同步加载
+        }
+      }
+    }
   },
   plugins: [
     new MiniCssExtractPlugin({
