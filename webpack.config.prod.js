8186ec06f84b1c58f2bb55e8733caa14860abf6d
yyzcl <yyzclyang@outlook.com>
2021-03-17 00:12:15 +0800

    chore(webpack): 打包时自动清空打包目录

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,3 +1,4 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { merge } = require('webpack-merge');
 const baseConfig = require('./webpack.config');
@@ -21,6 +22,7 @@ module.exports = merge(baseConfig, {
     }
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
       filename: 'static/css/index.[contenthash].css'
     })
