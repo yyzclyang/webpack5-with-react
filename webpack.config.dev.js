92bfff1e65a08cbb6d182791ed0c6dc77a85016e
yyzcl <yyzclyang@outlook.com>
2021-08-06 22:19:47 +0800

    chore(webpack): 设置 devServer

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,5 +2,15 @@ const { merge } = require('webpack-merge');
 const baseConfig = require('./webpack.config');
 
 module.exports = merge(baseConfig, {
-  mode: 'development'
+  mode: 'development',
+  devServer: {
+    clientLogLevel: 'warn',
+    historyApiFallback: true, // 在开发单页应用时非常有用，它依赖于HTML5 history API，如果设置为true，所有的跳转将指向index.html
+    compress: true, // 一切服务都启用 gzip 压缩
+    host: '0.0.0.0', // 外部网络可访问
+    open: true,
+    overlay: { warnings: false, errors: true }, // 在浏览器上全屏显示编译的errors或warnings。
+    port: 8080,
+    proxy: {},
+  },
 });
